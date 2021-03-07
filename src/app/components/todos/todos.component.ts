17ae538cfc20a619a6d29b047709311057693d95
elgnis3900 <jonathanduarte313@gmail.com>
2021-03-07 15:53:27 -0500

    todo list complete

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todos',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  todos: Todo[] | undefined;
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    // Delete from UI
+    this.todos = this.todos?.filter(t => t.id !== todo.id);
+    // Delete from server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo: Todo) {
+    this.todoService.addTodo(todo).subscribe((todo: any) => {
+      this.todos?.push(todo);
+    })
   }
 
 }
